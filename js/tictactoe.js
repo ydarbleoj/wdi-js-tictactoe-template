1ea37978ceb52ff0f37e4c63187e546b17da5974
ydarbleoj <brady.joel@gmail.com>
2014-03-16 13:03:32 -0700

    multiplayer done except play again prompt

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,116 +1,116 @@
-$(function() {
+// $(function() {
 
-  //### 'jquery' UI elements for game manipulation
-  //var game              =    // the game container
-  //var board             =    // the board  container
-  var status_indicators = $('#teams li');   // status bar container
+//   //### 'jquery' UI elements for game manipulation
+//   //var game              =    // the game container
+//   //var board             =    // the board  container
+//   var status_indicators = $('#teams li');   // status bar container
 
-  var tiles = [];                         // all the "tiles"
+//   var tiles = [];                         // all the "tiles"
 
-  var players = [                         // player data
-    {
-      name:      'Ernie',
-      marker:    '&times;',
-      img_url:   'img/ernie.jpg',
-      indicator: $(status_indicators[0])
-    },
-    {
-      name:      'Bert',
-      marker:    '&oslash;',
-      img_url:   'img/bert.jpg',
-      indicator: $(status_indicators[1])
-    }
-  ];
+//   var players = [                         // player data
+//     {
+//       name:      'Ernie',
+//       marker:    '&times;',
+//       img_url:   'img/ernie.jpg',
+//       indicator: $(status_indicators[0])
+//     },
+//     {
+//       name:      'Bert',
+//       marker:    '&oslash;',
+//       img_url:   'img/bert.jpg',
+//       indicator: $(status_indicators[1])
+//     }
+//   ];
 
-  var current_player;                     // player data
-  var turns  = 0;                         // elapsed turns
+//   var current_player;                     // player data
+//   var turns  = 0;                         // elapsed turns
 
-  //### There are eight winning combos, the first two are supplied.
-  //### What are the other six? Add 'em.
-  var win_combos = [
-    [0,1,2], [2, 5, 8], [0, 3, 6], [6, 7, 8], [0, 4, 8], [2, 4,6], [1, 4, 7], [3, 4, 5]
-  ];
+//   //### There are eight winning combos, the first two are supplied.
+//   //### What are the other six? Add 'em.
+//   var win_combos = [
+//     [0,1,2], [2, 5, 8], [0, 3, 6], [6, 7, 8], [0, 4, 8], [2, 4,6], [1, 4, 7], [3, 4, 5]
+//   ];
 
-  var initialize = function() {
-    //### ready the board for game play
+//   var initialize = function() {
+//     //### ready the board for game play
 
-    //### 1.) Create nine tiles. Each is a div, each needs to be bound to 'handle_click'.
-    //### Make sure giving each tile a unique 'id' for targeting. Find tile's 'class' in css.
-    //### Append tiles to board.
+//     //### 1.) Create nine tiles. Each is a div, each needs to be bound to 'handle_click'.
+//     //### Make sure giving each tile a unique 'id' for targeting. Find tile's 'class' in css.
+//     //### Append tiles to board.
 
-    //### 2.) Make first player the current_player
+//     //### 2.) Make first player the current_player
 
-    //### 3.) Set up the players 'indicators' in UI
-    //### - set player image, name, marker
-    //### - set player name
-    //### - the 'current_player' has a different style (see css '.current')
+//     //### 3.) Set up the players 'indicators' in UI
+//     //### - set player image, name, marker
+//     //### - set player name
+//     //### - the 'current_player' has a different style (see css '.current')
 
-    //### 4.) fade in the game
-  };
+//     //### 4.) fade in the game
+//   };
 
-  var handle_click = function() {
-    //### this function is bound to a click event for each tile on the board
-  }
+//   var handle_click = function() {
+//     //### this function is bound to a click event for each tile on the board
+//   }
 
-  var is_active = function(tile) {
-    //### boolean - is tile active?
-  };
+//   var is_active = function(tile) {
+//     //### boolean - is tile active?
+//   };
   
-  var activate_tile = function(tile) {
-    //### activate tile
-    //### don't forget to up 'turn' count
-  };
+//   var activate_tile = function(tile) {
+//     //### activate tile
+//     //### don't forget to up 'turn' count
+//   };
 
-  var toggle_player = function() {
-    //### After each turn, toggle the current player and update player indicators
-  };
+//   var toggle_player = function() {
+//     //### After each turn, toggle the current player and update player indicators
+//   };
 
 
-  var is_win = function() {
-    // ### whether or not the current player's positions result in a win
-    // ### returns boolean
-  };
+//   var is_win = function() {
+//     // ### whether or not the current player's positions result in a win
+//     // ### returns boolean
+//   };
 
-  var is_tie = function() {
-    //### has the game resulted in a tie?
-    //### returns boolean
-  };
+//   var is_tie = function() {
+//     //### has the game resulted in a tie?
+//     //### returns boolean
+//   };
 
-  var handle_win = function() {
-    //### update the UI to reflect that a win has occurred.
-    //### - show results panel
-    //### - display winner name and image
-    //### - congrats message
-    //### - show new_game button
+//   var handle_win = function() {
+//     //### update the UI to reflect that a win has occurred.
+//     //### - show results panel
+//     //### - display winner name and image
+//     //### - congrats message
+//     //### - show new_game button
 
-  };
+//   };
 
-  var handle_tie = function() {
-    //### update the UI to reflect that a tie game has occurred.
-    //### - show results panel
-    //### - display tie and rubber ducky image
-    //### - show new_game button
-  }
+//   var handle_tie = function() {
+//     //### update the UI to reflect that a tie game has occurred.
+//     //### - show results panel
+//     //### - display tie and rubber ducky image
+//     //### - show new_game button
+//   }
 
-  var hide_indicators = function() {
-    //### optional: call this to hide the "status" container after detecting a win or a tie
-  };
+//   var hide_indicators = function() {
+//     //### optional: call this to hide the "status" container after detecting a win or a tie
+//   };
 
-  var show_combo = function(combo) {
-    //### optional: call this to highlight the combination of tiles that resulted in a win
-    //### e.g. colors winning XXX or OOO red.
-  }
+//   var show_combo = function(combo) {
+//     //### optional: call this to highlight the combination of tiles that resulted in a win
+//     //### e.g. colors winning XXX or OOO red.
+//   }
 
-  var new_game = function() {
-    // see http://stackoverflow.com/questions/2405117/difference-between-window-location-href-window-location-href-and-window-location
-    // nothing to add here
-    window.location.href = window.location.href
-  };
+//   var new_game = function() {
+//     // see http://stackoverflow.com/questions/2405117/difference-between-window-location-href-window-location-href-and-window-location
+//     // nothing to add here
+//     window.location.href = window.location.href
+//   };
 
-  // call initialize() to get the party started
-  initialize();
+//   // call initialize() to get the party started
+//   initialize();
 
-});
+// });
 
 
 
--- js/tttApp.js
+++ js/tttApp.js
@@ -2,56 +2,40 @@ var tttApp = angular.module("tttApp", []);
 
 tttApp.controller('myCtrl', ['$scope', function($scope){
 
-	$scope.message = "Tic Tac Toe";
-
-	$scope.players = [                         // player data
+	$scope.players = [                  
     {
       name:      'Ernie',
-      marker:    '&times;',
+      symbol:    'X',
       img_url:   'img/ernie.jpg',
+      tiles:     []
     },
     {
       name:      'Bert',
-      marker:    '&oslash;',
+      symbol:    'O',
       img_url:   'img/bert.jpg',
+      tiles:     []
     }
   ];
 
   $scope.tiles = [
- 		{letter: "", id: 0, active: false }, 
-  	{letter: "", id: 1, active: false }, 
-  	{letter: "", id: 2, active: false }, 
-  	{letter: "", id: 3, active: false },
-  	{letter: "", id: 4, active: false },
-  	{letter: "", id: 5, active: false },
-  	{letter: "", id: 6, active: false },
-  	{letter: "", id: 7, active: false },
-  	{letter: "", id: 8, active: false }
-  ]
-
+ 		{ letter: "", id: 0, active: false },
+ 		{ letter: "", id: 1, active: false },
+  	{ letter: "", id: 2, active: false }, 
+  	{ letter: "", id: 3, active: false },
+  	{ letter: "", id: 4, active: false },
+  	{ letter: "", id: 5, active: false },
+  	{ letter: "", id: 6, active: false },
+  	{ letter: "", id: 7, active: false },
+  	{ letter: "", id: 8, active: false }
+  ];
 
   $scope.winCombos = [
     [0,1,2], [2, 5, 8], [0, 3, 6], [6, 7, 8], [0, 4, 8], [2, 4,6], [1, 4, 7], [3, 4, 5]
   ];
 
-
-  var include = function(value, index) {
-  	return array.indexOf(value) > -1;  
-  }
-
-  var fullTile = function() {
-  	var fullTiles = [];
-  	var tiles = $scope.tiles;
-  	_.each(tiles, function(tile){
-  		if(tile.active === true){
-  			fullTiles.push(tile);
-	  	}
-  	});
-  };
-
   $scope.currentPlayer = $scope.players[0];
 
-  var togglePlayer = function(currentPlayer){
+  var togglePlayer = function(){
   	if($scope.currentPlayer === $scope.players[0]){
   		$scope.currentPlayer = $scope.players[1];
   	} else {
@@ -59,17 +43,80 @@ tttApp.controller('myCtrl', ['$scope', function($scope){
   	}
   };
 
+  $scope.clickTile = function(id){
+  	var tiles = $scope.tiles;
+  	var tile = $.grep(tiles, function(t) { return t.id == id;})[0];
+  	if (tile.active === false){
+  		var player = $scope.currentPlayer;
+  		tile.letter = player.symbol;
+  		tile.active = true;
+  		player.tiles.push(id);
+  		var status = checkGameStatus(player);
+  		if(status === 'you win'){
+  			winGame(player);
+  		} else {
+  			tieGame();
+  		}
+  	}
+  		togglePlayer();
+  };
 
-  // var clickTile = function(){ 
-  // 	var 
 
-  // };
+  var winGame = function(player) {
+  	console.log(player + "you win" )
+  	console.log( 'winning line is:' + $scope.winningLine)
+  }
+
+  var tieGame = function() {
+  	console.log("You Tied!")
+  }
 
 
+  // seems to be working 
+  var fullBoard = function() {
+  	var tilesFull = [];
+  	var tiles = $scope.tiles;
+  	_.each(tiles, function(tile){
+  		if(tile.active === true){
+  			tilesFull.push(tile);
+	  	}
+  	})
+  	return tilesFull.length === 9;
+  }
+
+  $scope.winningLine;
+  var checkGameStatus = function(player){
+  	var status;
+  	var lines = $scope.winCombos;
+  	if ( fullBoard() ) {
+  		status = "tied";
+  	}
+  	_.each(lines, function(line){
+  		if( include( line[0], player.tiles) && include(line[1], player.tiles) && include(line[2], player.tiles) ) {
+  			status = "you win";
+  			$scope.winningLine = line;
+  		}
+  	});
+	return status;
+  }
 
 
+   var initialize = function(){
+    $scope.currentPlayer = $scope.players[0];
+    _.each($scope.tiles, function(tile){
+      tile.letter = "";
+      tile.active = false;
+    })
+    _.each($scope.players, function(play){
+      play.tiles = [];
+    })
+    console.log("New game...")
+  }
 
-
+   var include = function(value, array) {
+  	return array.indexOf(value) > -1;
+  	console.log(array)  
+  }
 
 
 
